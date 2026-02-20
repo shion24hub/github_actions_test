7881c983d64e4053a90327c86bbfda78b9f71434
Shion Noguchi <shion24stat@gmail.com>
2026-02-20 12:32:44 +0900

    Fix.

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+from datetime import datetime
+
+
+def main():
+    print("Hello World!")
+    print(f"Current time: {datetime.now()}")
+    time.sleep(1)
+    print("Goodbye World!")
+
+
+if __name__ == "__main__":
+    main()
